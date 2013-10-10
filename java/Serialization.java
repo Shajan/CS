045b1ca696b0534eecc927ec7d547ef44b05a5f1
Shajan Dasan <sdasan@twitter.com>
2013-10-09 17:05:25 -0700

    add perf tests

--- java/Serialization.java
+++ java/Serialization.java
@@ -4,22 +4,82 @@ class Test {
     public static void main(String args[]) {
         String fileName = "data";
         Serialization s = new Serialization();
-            s.name = "Shajan Dasan";
-            s.address = "Sammamish, WA";
-            s.SSN = 123456789;
-            s.number = 747;
+        Init(s);
+
+/*
         System.out.println("-------Serialization:Original object");
         s.print();
         ReadWrite(s, fileName + ".ser");
+*/
 
         External e = new External();
-            e.name = s.name;
-            e.address = s.address;
-            e.SSN = s.SSN;
-            e.number = s.number;
+        Init(e);
+/*
         System.out.println("-------Externalization:Original object");
         e.print();
         ReadWrite(e, fileName + ".ext");
+*/
+        int n = 1000000;
+
+        Serialization sa[] = new Serialization[n];
+        External ea[] = new External[n];
+
+        for (int i=0; i<n; ++i) {
+            sa[i] = s;
+            ea[i] = e;
+        }
+
+        long serTime = PerfTest(sa, fileName + ".ser");
+        long extTime = PerfTest(ea, fileName + ".ext");
+
+        if (extTime > serTime) {
+            System.out.println("Serialization is faster by (ns): " + (extTime - serTime));
+            System.out.println("Serialization is faster by (%): " + 100*(extTime - serTime)/extTime);
+        } else {
+            System.out.println("Externalization is faster by (ns): " + (serTime - extTime));
+            System.out.println("Serialization is faster by (%): " + 100*(serTime - extTime)/serTime);
+        }
+    }
+
+    private static void Init(Record r) {
+        r.type = "faces"; r.x = 500; r.y = 300; r.w = 68; r.h = 72;
+    }
+
+    private static long PerfTest(Record r[], String fileName) {
+        long timetowrite = 0;
+        long timetoread = 0;
+        try
+        {
+            FileOutputStream fileOut = new FileOutputStream(fileName);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            timetowrite = System.nanoTime();
+            for (int i=0; i<r.length; ++i)
+                out.writeObject(r[i]);
+            timetowrite = System.nanoTime() - timetowrite;
+            out.close();
+            fileOut.close();
+
+            FileInputStream fileIn = new FileInputStream(fileName);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            timetoread = System.nanoTime();
+            for (int i=0; i<r.length; ++i)
+                r[i] = (Record) in.readObject(); 
+            timetoread = System.nanoTime() - timetoread;
+            in.close();
+            fileIn.close();
+            System.out.println("--------- " + fileName);
+            System.out.println("Read time (ns)" + timetoread);
+            System.out.println("Write time (ns)" + timetowrite);
+        } catch(IOException i) {
+            i.printStackTrace();
+            return 0;
+        } catch(ClassNotFoundException c) {
+            System.out.println("class not found");
+            c.printStackTrace();
+            return 0;
+        }
+
+        return timetowrite + timetoread;
     }
 
     private static void ReadWrite(Record r, String fileName) {
@@ -51,18 +111,20 @@ class Test {
 }
 
 class Record implements Serializable {
-    public String name;
-    public String address;
-    public transient int SSN;
-    public int number;
+    public int x;
+    public int y;
+    public int w;
+    public int h;
+    public String type;
 
     public Record() {}
  
     public void print() {
-        System.out.println("Name: " + name);
-        System.out.println("Address: " + address);
-        System.out.println("SSN: " + SSN);
-        System.out.println("Number: " + number);
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("w: " + w);
+        System.out.println("h: " + h);
+        System.out.println("Type: " + type);
     }
 }
 
@@ -73,15 +135,19 @@ class External extends Record implements Externalizable {
     public External() { super(); }
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
-        out.writeObject(name);
-        out.writeObject(address);
-        out.writeInt(number);
+        out.writeInt(x);
+        out.writeInt(y);
+        out.writeInt(w);
+        out.writeInt(h);
+        out.writeObject(type);
     }
 
     @Override
     public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-        name    = (String) in.readObject();
-        address = (String) in.readObject();
-        number  = in.readInt();
+        x    = in.readInt();
+        y    = in.readInt();
+        w    = in.readInt();
+        h    = in.readInt();
+        type = (String) in.readObject();
     }
 }
