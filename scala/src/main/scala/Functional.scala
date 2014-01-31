1c6a3490af157c0d5487cbdbec460a085b760c2a
Shajan Dasan <sdasan@twitter.com>
2014-01-31 11:33:01 -0800

    checkpoint

--- scala/src/main/scala/Functional.scala
+++ scala/src/main/scala/Functional.scala
@@ -1,20 +1,29 @@
 object Functional {
+
+  // example : toStringAll(List(1, "hello", ('a', List(2, 3, 4), 5)))
   def toStringAll(list: List[Any]): List[String] =
     list match {
+      // case x :: y matches atleast one element x in the list y can be Nil
+      // => a :: b '::' is the concatination operation
+      //   method name is '::', any method name ending with ':' is right associated
+      //   equivalent to b.::(a) where '::' is the function name
+      //   rhs 'b' is the object whose method is called
+      //
+      // Nil is the empty list, a singleton object
       case head :: tail => head.toString :: toStringAll(tail)
       case Nil => Nil
     }
 
   def transformHugeStack[A, B](list: List[A], f: Function[A, B]): List[B] =
     list match {
-      case head :: tail => f(head) :: transform(tail, f)
+      case head :: tail => f(head) :: transformHugeStack(tail, f)
       case Nil => Nil
     }
 
   def transform[A, B](list: List[A], f: Function[A, B]): List[B] = {
     def go(list: List[A], f: Function[A, B], acc: List[B]): List[B] =
       list match {
-        case head :: tail => go(tail, f, f(head) :: acc) // simulate tail reccursion by scala compiler
+        case head :: tail => go(tail, f, f(head) :: acc) // tail reccursion by scala compiler
         case Nil => acc.reverse
       }
     go(list, f, List.empty)
