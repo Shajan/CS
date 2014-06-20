645a0d1feb7e8467ae95040e9f08451365dda63f
Shajan Dasan <sdasan@twitter.com>
2014-06-20 14:48:57 -0700

    Add more bash examples

--- bash/sample.sh
+++ bash/sample.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 #Check number of command line args
+#set -x #echo lines for debugging
 [ "$#" -eq 3 ] || { echo "Usage :" `basename $0` "YYYY MM DD" >&2; exit 1; }
 
 #Get command line args
@@ -11,4 +12,22 @@ DAY=$3
 for HOUR in 0{0..9} {10..11}
 do
   echo "$YEAR/$MONTH/$DAY $HOUR"
+  SUM=0
+  for VER in {13..14}
+  do
+        CUR=`egrep "foo" input.txt | wc -l`
+        SUM=`expr $SUM + $CUR`
+  done
+  RESULT="$RESULT$SUM "
 done
+
+function foo_bar()
+{
+  local var1=$1
+  echo $var1
+  RESULT="test"
+}
+
+RESULT=""
+foo_bar abc
+echo $RESULT
