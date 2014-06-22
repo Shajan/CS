fbb6400ae7055b4338c0caae9d43a301d7f7fb74
Shajan Dasan <sdasan@gmail.com>
2014-06-21 20:31:17 -0700

    remove tabs

--- OpenCV/PlayMovie.cpp
+++ OpenCV/PlayMovie.cpp
@@ -2,21 +2,21 @@
 
 int PlayMovie(const char* fileName) {
     cvNamedWindow("Movie", CV_WINDOW_AUTOSIZE);
-	CvCapture* capture = cvCreateFileCapture(fileName);
-	IplImage* frame;
-	while (true) {
-		frame = cvQueryFrame(capture);
-		if (frame == NULL)
-			break;
-		cvShowImage("Movie", frame);
+    CvCapture* capture = cvCreateFileCapture(fileName);
+    IplImage* frame;
+    while (true) {
+        frame = cvQueryFrame(capture);
+        if (frame == NULL)
+            break;
+        cvShowImage("Movie", frame);
 
-		// 'ESC' key for exit
-		// Assuming at 30fps, check frame rate to be sure
-		char c = cvWaitKey(33);
-		if (c == 27)
-			break;
-	}
-	cvReleaseCapture(&capture);
-	cvDestroyWindow("Movie");
-	return 0; 
+        // 'ESC' key for exit
+        // Assuming at 30fps, check frame rate to be sure
+        char c = cvWaitKey(33);
+        if (c == 27)
+            break;
+    }
+    cvReleaseCapture(&capture);
+    cvDestroyWindow("Movie");
+    return 0; 
 }
--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -9,14 +9,14 @@ char* image = "..\\opencv-logo.png";
 char* video = "..\\test-video.mov";
 
 int main(int argc, char **argv) {
-	const char* fileName = NULL;
-  	if (argc == 2) {
-		fileName = argv[1];
-	} else {
+    const char* fileName = NULL;
+    if (argc == 2) {
+        fileName = argv[1];
+    } else {
         fileName = video;
     }
-	
-	//return TestMain(fileName);
-	//return ReadImageMain(fileName);
-	return PlayMovie(fileName);
+    
+    //return TestMain(fileName);
+    //return ReadImageMain(fileName);
+    return PlayMovie(fileName);
 }
