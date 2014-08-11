e34a64e76c385e71730f30dfd63ae98d8f16169f
Shajan Dasan <sdasan@twitter.com>
2014-08-11 16:10:06 -0700

    Add thrift sample

--- thrift/mk.sh
+++ thrift/mk.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+if [ "$1" == "clean" ]; then
+  rm sample
+  rm -rf gen-cpp
+  rm -rf sample.dSYM
+  exit 0
+fi
+
+# generate cpp files
 thrift --gen cpp sample.thrift
+
+# -g : source level debug info
+# thrift include folder : /usr/local/include/thrift
+# boost include folder : /usr/local/include/boost
+cc -g -I /usr/local/include/thrift -I /usr/local/include/boost -o sample sample.cpp
+
---
+++ thrift/sample.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include <boost/shared_ptr.hpp>
+
+#include <protocol/TBinaryProtocol.h>
+#include <protocol/TDenseProtocol.h>
+#include <protocol/TJSONProtocol.h>
+#include <transport/TTransportUtils.h>
+#include <transport/TFDTransport.h>
+
+#include "gen-cpp/KeyValService.h"
+
+int main(int argc, char* argv[]) {
+    printf("Hello World!\n");
+    getchar();
+}
