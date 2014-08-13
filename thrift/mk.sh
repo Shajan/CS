5e8724789c0ba6f3f342ef52c2988b69cbf5fe22
Shajan Dasan <sdasan@twitter.com>
2014-08-12 17:37:55 -0700

    Add protocol and transport

--- thrift/mk.sh
+++ thrift/mk.sh
@@ -13,4 +13,6 @@ thrift --gen cpp sample.thrift
 # -g : Source level debug info
 # -I : Include folders
 # -L : Lib folder
-clang++ -g -I /usr/local/include/thrift -I /opt/twitter/Cellar/boost/1.54.0/include/boost/ -o sample sample.cpp -L/opt/twitter/Cellar/boost/1.54.0/lib
+# -l : Thrift lib
+clang++ -g -I /usr/local/include/thrift -I /opt/twitter/Cellar/boost/1.54.0/include/boost/ -o sample sample.cpp gen-cpp/sample_types.cpp -L/opt/twitter/Cellar/boost/1.54.0/lib -lthrift
+
--- thrift/sample.cpp
+++ thrift/sample.cpp
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <iostream>
 
 #include <boost/shared_ptr.hpp>
-
+/*
 #include <protocol/TBinaryProtocol.h>
 #include <protocol/TDenseProtocol.h>
-#include <protocol/TJSONProtocol.h>
 #include <transport/TTransportUtils.h>
 #include <transport/TFDTransport.h>
+*/
+
+#include <protocol/TJSONProtocol.h>
+#include <transport/TFileTransport.h>
 
 #include "gen-cpp/KeyValService.h"
 
-#define FILE_NAME "data.bin"
+#define FILE_NAME "/tmp/data.bin"
+
+using namespace boost;
+using namespace std;
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
 
 void read_from(const char* file_name, KeyVal& kv);
 void write_to(const char* file_name, KeyVal& kv);
@@ -22,21 +32,34 @@ int main(int argc, char* argv[]) {
     const char * file_name = FILE_NAME;
     KeyVal kv;
 
-    if (argc > 2 && strcmp(argv[1], "write")) {
+    if (argc > 1 && (strcmp(argv[1], "write") == 0)) {
         read = false; 
     }
 
     if (read) {
       read_from(file_name, kv);
+      printf("key(%s), val(%s)", kv.key.c_str(), kv.val.c_str());
     } else {
+      kv.key = "Name";
+      kv.val = "Shajan Dasan";
       write_to(file_name, kv);
     }
     return 0;
 }
 
 void read_from(const char* file_name, KeyVal& kv) {
+	shared_ptr<TFileTransport> transport(new TFileTransport(file_name, true));
+	TJSONProtocol protocol(transport);
+	transport->open();
+	kv.read(&protocol);
+	transport->close();
 }
 
 void write_to(const char* file_name, KeyVal& kv) {
+	shared_ptr<TFileTransport> transport(new TFileTransport(file_name, false));
+	TJSONProtocol protocol(transport);
+	transport->open();
+	kv.write(&protocol);
+	transport->close();
 }
 
