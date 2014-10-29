785327b935c59f265d6f47a095a4df20dd38bb29
Shajan Dasan <sdasan@gmail.com>
2014-10-28 23:27:43 -0700

    Rename currying example

--- scala/Currying.scala
+++ scala/Currying.scala
@@ -1,11 +1,11 @@
 object Currying {
   def main(args: Array[String]) {
     val nums = List(1, 2, 3, 4, 5, 6, 7, 8)
-    println(filter(nums, modN(2))) // List(2, 4, 6, 8)
-    println(filter(nums, modN(3))) // List(3, 6)
+    println(filter(nums, isDivisibleBy(2))) // List(2, 4, 6, 8)
+    println(filter(nums, isDivisibleBy(3))) // List(3, 6)
   }
 
-  def modN(n: Int)(x: Int) = ((x % n) == 0)
+  def isDivisibleBy(n: Int)(x: Int) = ((x % n) == 0)
 
   def filter(l: List[Int], f: Int => Boolean): List[Int] = {
     if (l.isEmpty) l
