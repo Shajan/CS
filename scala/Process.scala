de1b142bf5c3937b9aa893b8e9ee76196f913953
Shajan Dasan <sdasan@twitter.com>
2014-11-02 10:01:34 -0800

    Add process, pipe

---
+++ scala/Process.scala
@@ -0,0 +1,32 @@
+import scala.sys.process._
+
+object Process {
+  def main(args: Array[String]) {
+    //simple
+    advanced
+    exit
+    error
+  }
+
+  def simple = {
+    "ls".!
+    println("...........")
+    println(Seq("ls", "/tmp").!!)
+    println("...........")
+  }
+
+  def advanced = {
+    println("...........")
+    // pipe
+    "ls"  #| "wc"
+    println("...........")
+  }
+
+  def exit = {
+    System.exit(0)
+  }
+
+  def error = {
+    println("Error: should never get here!!")
+  }
+}
