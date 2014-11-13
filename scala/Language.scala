aa7868ac89d4fc94848cf8e3d8cc36e8673a880f
Shajan Dasan <sdasan@twitter.com>
2014-11-13 14:17:10 -0800

    Add traits

--- scala/Language.scala
+++ scala/Language.scala
@@ -1,9 +1,10 @@
 object Language {
   def main(args: Array[String]) {
-    loops()
+    //loops
+    traits
   }
 
-  def loops() = {
+  def loops = {
     var i=3 
     loop {
       println("i = " + i)
@@ -19,5 +20,24 @@ object Language {
         if (!cond) unless(cond)
       }
     }
+  }
+
+  class Ball {
+    def properties(): List[String] = List()
+    override def toString() = "It's a" +
+      properties.mkString(" ", ", ", " ") +
+      "ball"
+  }
+
+  trait Red extends Ball {
+    override def properties() = super.properties ::: List("red")
+  }
+
+  trait Shiny extends Ball {
+    override def properties() = super.properties ::: List("shiny")
+  }
+
+  def traits = {
+    println(new Ball with Shiny with Red) // It's a shiny, red ball
   }
 }
