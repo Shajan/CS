00050fee8b76e82353953c01b9bbc38e797749cc
Shajan Dasan <sdasan@twitter.com>
2014-11-16 10:44:01 -0800

    Add non console logging

--- scala/Log.scala
+++ scala/Log.scala
@@ -2,20 +2,45 @@ import java.util.logging._
 
 object Log {
   def main(args: Array[String]) {
-    val t = new MyTest
-    t.test
+    Console.log
+    NoConsole.log
   }
 }
 
-class MyTest {
+object Console {
   val logger = Logger.getLogger(this.getClass.getName)
-  def test = {
-    logger.setLevel(Level.WARNING);  // Log warning or higher
+  logger.setLevel(Level.WARNING) // Log warning or higher
+  def log = {
     // In decreasing order of severity
     logger.finest("hello finest")
     logger.finer("hello finer")
     logger.fine("hello fine")
     logger.config("hello config")
+    logger.warning("hello warning")
+    logger.info("hello info")
+    logger.severe("hello severe")
+  }
+}
+
+object NoConsole {
+/* 
+//To globally remove handlers
+val globalLogger = Logger.getLogger("global")
+val handlers = globalLogger.getHandlers()
+for (handler <- handlers)
+    globalLogger.removeHandler(handler)
+*/
+  val logger = Logger.getLogger(this.getClass.getName)
+  logger.setUseParentHandlers(false)
+  logger.setLevel(Level.WARNING)
+
+  // create a TXT formatter
+  val formatter = new SimpleFormatter
+  val filehandler = new FileHandler("/tmp/Logging.txt")
+  filehandler.setFormatter(formatter)
+  logger.addHandler(filehandler)
+
+  def log = {
     logger.warning("hello warning")
     logger.info("hello info")
     logger.severe("hello severe")
