56a6800bbbf146e328839ddf310876bb1fa4b5af
Shajan Dasan <sdasan@twitter.com>
2014-11-16 12:25:19 -0800

    Add parse example

--- scala/RegEx.scala
+++ scala/RegEx.scala
@@ -5,6 +5,13 @@ object RegEx {
     val pattern = new Regex("(S|s)cala")
     val str = "Scala is scalable and cool"
     println((pattern findAllIn str).mkString(",")) // Scala,scala
+    println(pattern replaceFirstIn(str, "Java")) // Java is scalable and cool
+
+    val p = "Sleep ([0-9]*)$".r
+    "Sleep 100" match {
+      case p(n) => println(n.toInt) // prints 100
+      case _ => println("no match")
+    }
   }
 }
 
