552f5388db3fec0c9d5895388b171c6676756bbf
Shajan Dasan <sdasan@twitter.com>
2015-03-20 09:44:23 -0700

    Add socket transport

--- java/ipc/ipc.java
+++ java/ipc/ipc.java
@@ -1,18 +1,17 @@
 /*
- * Benchmark different ipc transports - stdio, shared memory
+ * Benchmark different ipc transports - stdio, shared memory, socket
  *
  * NOTE:
  *   Self contained single source file.
  *   Commandline determines mode (client vs. server).
  *   Server will launch the client.
  *
- * Author Shajan Dasan (sdasan@gmail.com)
+ * Shajan Dasan (sdasan@gmail.com)
  */
 import java.io.*;
-import java.nio.ByteBuffer;;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.concurrent.Semaphore;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
 import java.util.Random;
 
 class ipc {
@@ -20,12 +19,17 @@ class ipc {
   private static int sDataMultiplier = 1024; // sBufferSize x sDataMultiplier gives the total payload
   private static int sIterations = 10;       // Number of times to send total payload
   private static String sFileName = "data";  // Used as backing file for shared memory
+  private static int sPort = 8080;           // TCP server port for socket
+  private static byte[] sBuffer;             // Static buffer for read/write
+  private static ByteBuffer sByteBuffer;     // Static ByteBuffer for read/write
+  private static Process sClientProcess;     // Client process launched by server, only valid in server mode
+  private static boolean fNio = false;       // Force using nio (channel & ByteBuffer) for stdio
   private static boolean sDebug = false;
-  private static byte[] sBuffer = null;      // Static buffer for read/write
 
   public static void main(String args[]) {
     ITransport transport = new stdio();
     boolean client = false;
+    boolean direct = false;
 
     try {
       for (int i=0; i<args.length; ++i) {
@@ -33,22 +37,30 @@ class ipc {
           case "-client" : client = true; break;
           case "-stdio" : transport = new stdio(); break;
           case "-memmap" : transport = new memmap(); break;
+          case "-socket" : transport = new socket(); break;
+          case "-direct" : direct = true; break;
+          case "-nio" : fNio = true; break;
           case "-debug" : sDebug = true; break;
           case "-b" : sBufferSize = Integer.parseInt(args[++i]); break;
           case "-m" : sDataMultiplier = Integer.parseInt(args[++i]); break;
           case "-i" : sIterations = Integer.parseInt(args[++i]); break;
+          case "-p" : sPort = Integer.parseInt(args[++i]); break;
           case "-f" : sFileName = args[++i]; break;
           default : throw new IllegalArgumentException("Unknown option : " + args[i]);
         }
       }
     } catch (Exception e) {
-      log("Usage : ipc [-client] [-stdio|-memmap] [-debug] [-f fileName] [-b bufferSize] [-m dataMultiplier] [-i iterations]");
+      log("Usage : ipc [-client] [-stdio|-memmap] [-debug] [-direct] [-nio] [-p port] [-f fileName] [-b bufferSize] [-m dataMultiplier] [-i iterations]");
       logException(e);
       return;
     }
 
     try {
       sBuffer = new byte[sBufferSize];
+      if (direct)
+        sByteBuffer = ByteBuffer.allocateDirect(sBufferSize);
+      else
+        sByteBuffer = ByteBuffer.allocate(sBufferSize);
       if (client) {
         transport.client();
       } else { 
@@ -59,15 +71,15 @@ class ipc {
           cmdLine.append(args[i]);
         }
         ipc.debugLog("Start client..");
-        Process p = Runtime.getRuntime().exec(cmdLine.toString());
-        logInputStream(p.getErrorStream(), true);
-        logInputStream(p.getInputStream(), false);
+        sClientProcess = Runtime.getRuntime().exec(cmdLine.toString());
+        logInputStream(sClientProcess.getErrorStream(), true);
+        logInputStream(sClientProcess.getInputStream(), false);
         Random random = new Random(System.currentTimeMillis());
         random.nextBytes(sBuffer);
         ipc.debugLog("Start server..");
-        transport.server(p);
+        transport.server();
         ipc.debugLog("End server..");
-        p.destroy();
+        sClientProcess.destroy();
         ipc.debugLog("Kill client..");
       }
     } catch (Exception e) {
@@ -76,54 +88,46 @@ class ipc {
   }
 
   interface ITransport {
-    void server(Process clientProcess) throws IOException;
+    void server() throws IOException;
     void client() throws IOException;
-  }
-
-  static class memmap implements ITransport {
-    @Override
-    public void server(Process clientProcess) throws IOException {
-      File f = new File(ipc.sFileName);
-      FileChannel fc = new RandomAccessFile(f, "rw").getChannel();
-      final MappedByteBuffer mem = fc.map(FileChannel.MapMode.READ_WRITE, 0, sBufferSize * sDataMultiplier);
-      for (int i=0; i<sIterations; ++i)
-        new ipc.timed("memmap.write"){{ write(mem.duplicate()); }}.end();
-      fc.close();
-    }
-
-    @Override
-    public void client() throws IOException {
-      File f = new File(ipc.sFileName);
-      FileChannel fc = new RandomAccessFile(f, "r").getChannel();
-      final MappedByteBuffer mem = fc.map(FileChannel.MapMode.READ_ONLY, 0, sBufferSize * sDataMultiplier);
-      for (int i=0; i<sIterations; ++i)
-        new ipc.timed("memmap.read"){{ read(mem.duplicate()); }}.end();
-      fc.close();
-    }
-
-    private void write(ByteBuffer bb) throws IOException {
-      for (int i=0; i<ipc.sDataMultiplier; ++i)
-        bb.put(ipc.sBuffer);
-    }
-
-    private void read(ByteBuffer bb) throws IOException {
-      for (int i=0; i<ipc.sDataMultiplier; ++i)
-        bb.get(ipc.sBuffer);
-    }
   }
 
   static class stdio implements ITransport {
     @Override
-    public void server(Process clientProcess) throws IOException {
-      final OutputStream os = clientProcess.getOutputStream();
-      for (int i=0; i<sIterations; ++i)
-        new ipc.timed("stdio.write"){{ write(os); }}.end();
+    public void server() throws IOException {
+      final OutputStream os = sClientProcess.getOutputStream();
+      final WritableByteChannel channel = Channels.newChannel(os);
+      if (fNio) {
+        for (int i=0; i<sIterations; ++i)
+          new ipc.timed("stdio.write"){{ write(channel); }}.end();
+      } else {
+        // Duplicating for loop to reduce perf variations, useful when comparing with other transports
+        for (int i=0; i<sIterations; ++i)
+          new ipc.timed("stdio.write"){{ write(os); }}.end();
+      }
     }
 
     @Override
     public void client() throws IOException {
-      for (int i=0; i<sIterations; ++i)
-        new ipc.timed("stdio.read"){{ read(System.in); }}.end();
+      final ReadableByteChannel channel = Channels.newChannel(System.in);
+      if (fNio) {
+        for (int i=0; i<sIterations; ++i)
+          new ipc.timed("stdio.read"){{ read(channel); }}.end();
+      } else {
+        // Duplicating for loop to reduce perf variations, useful when comparing with other transports
+        for (int i=0; i<sIterations; ++i)
+          new ipc.timed("stdio.read"){{ read(System.in); }}.end();
+      }
+    }
+
+    private void write(WritableByteChannel channel) throws IOException {
+      for (int i=0; i<ipc.sDataMultiplier; ++i)
+        channel.write(ipc.getByteBuffer());
+    }
+
+    private void read(ReadableByteChannel channel) throws IOException {
+      for (int i=0; i<ipc.sDataMultiplier; ++i)
+        channel.read(ipc.getByteBuffer());
     }
 
     private void write(OutputStream os) throws IOException {
@@ -150,6 +154,69 @@ class ipc {
     }
   }
 
+  static class memmap implements ITransport {
+    @Override
+    public void server() throws IOException {
+      File f = new File(ipc.sFileName);
+      FileChannel fc = new RandomAccessFile(f, "rw").getChannel();
+      final MappedByteBuffer mem = fc.map(FileChannel.MapMode.READ_WRITE, 0, sBufferSize * sDataMultiplier);
+      for (int i=0; i<sIterations; ++i)
+        new ipc.timed("memmap.write"){{ write(mem.duplicate()); }}.end();
+      fc.close();
+    }
+
+    @Override
+    public void client() throws IOException {
+      // TODO: Cross process synchronize read/write, ok for now as we are interested only in raw throuput
+      File f = new File(ipc.sFileName);
+      FileChannel fc = new RandomAccessFile(f, "r").getChannel();
+      final MappedByteBuffer mem = fc.map(FileChannel.MapMode.READ_ONLY, 0, sBufferSize * sDataMultiplier);
+      for (int i=0; i<sIterations; ++i)
+        new ipc.timed("memmap.read"){{ read(mem.duplicate()); }}.end();
+      fc.close();
+    }
+
+    private void write(ByteBuffer bb) throws IOException {
+      for (int i=0; i<ipc.sDataMultiplier; ++i)
+        bb.put(ipc.sBuffer);
+    }
+
+    private void read(ByteBuffer bb) throws IOException {
+      for (int i=0; i<ipc.sDataMultiplier; ++i)
+        bb.get(ipc.sBuffer);
+    }
+  }
+
+  static class socket implements ITransport {
+    @Override
+    public void server() throws IOException {
+      final SocketChannel sc = SocketChannel.open();
+      sc.connect(new InetSocketAddress("localhost", ipc.sPort));
+      for (int i=0; i<sIterations; ++i)
+        new ipc.timed("socket.write"){{ write(sc); }}.end();
+      sc.close();
+    }
+
+    @Override
+    public void client() throws IOException {
+      final SocketChannel sc = SocketChannel.open();
+      sc.connect(new InetSocketAddress("localhost", ipc.sPort));
+      for (int i=0; i<sIterations; ++i)
+        new ipc.timed("socket.read"){{ read(sc); }}.end();
+      sc.close();
+    }
+
+    private void write(SocketChannel sc) throws IOException {
+      for (int i=0; i<ipc.sDataMultiplier; ++i)
+        sc.write(ipc.getByteBuffer());
+    }
+
+    private void read(SocketChannel sc) throws IOException {
+      for (int i=0; i<ipc.sDataMultiplier; ++i)
+        sc.read(ipc.getByteBuffer());
+    }
+  }
+
   static class timed {
     long start;
     String tag;
@@ -163,6 +230,11 @@ class ipc {
       long duration = System.nanoTime() - start;
       ipc.log(tag + " " + duration/(1000*1000) + " ms");
     }
+  }
+
+  static ByteBuffer getByteBuffer() {
+    sByteBuffer.clear();
+    return sByteBuffer;
   }
 
   static void errorLog(String s) {
--- java/ipc/run.sh
+++ java/ipc/run.sh
@@ -1,7 +1,17 @@
-#Usage : ipc [-client] [-stdio|-memmap] [-debug] [-f fileName] [-b bufferSize] [-m dataMultiplier] [-i iterations]
+#Usage : ipc [-client] [-stdio|-memmap] [-debug] [-direct] [-nio] [-p port] [-f fileName] [-b bufferSize] [-m dataMultiplier] [-i iterations]
+
+echo "Effect of stream vs nio vs direct buffer"
+echo "streams"
+java ipc -stdio -i 5 -b 100000
+echo "nio"
+java ipc -stdio -nio -i 5 -b 100000
+echo "direct buffer"
+#java ipc -stdio -direct -nio -i 5 -b 100000
+
+echo "Effect of buffer size"
 for bufferSize in 10000 100000 1000000
 do
   echo Buffer size : $bufferSize
-  java ipc -stdio -i 5 -b $bufferSize
-  java ipc -memmap -f ./data -i 5 -b $bufferSize
+  #java ipc -stdio -i 5 -b $bufferSize
+  #java ipc -memmap -f ./data -i 5 -b $bufferSize
 done
