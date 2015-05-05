5de155bab86261b075ba108874b2ca8001ec106f
Shajan Dasan <sdasan@twitter.com>
2015-05-05 13:26:21 -0700

    Add more tail reccursion

--- scala/TailRec.scala
+++ scala/TailRec.scala
@@ -19,8 +19,21 @@ object TailRec {
     // Use an accumulator to hold the result of that computation instead
   }
  */
+
+  def reverse(l1: List[Int], l2: List[Int]): List[Int] = {
+    if (l1.size == 0) l2
+    else reverse(l1.tail, l1.head :: l2)
+  }
+
   def main(args: Array[String]): Unit = {
     println(fact(10))
+
+    val l1 = List(1,2,3,4,5,6,7)
+    val l2 = reverse(l1, Nil)
+
+    println(l1.mkString(","))
+    println(l2.mkString(","))
   }
+
 }
 
