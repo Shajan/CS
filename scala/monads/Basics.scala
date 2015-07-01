2d4343fef5bb485425ad71611c200f0ba00f5742
Shajan Dasan <sdasan@twitter.com>
2015-07-01 16:50:37 -0700

    For comprehension

--- scala/monads/Basics.scala
+++ scala/monads/Basics.scala
@@ -1,9 +1,10 @@
 object Basics {
   def main(args: Array[String]) {
-     basics()
+     //mapFlatMap()
+     forComprehension()
   }
 
-  def basics() = {
+  def mapFlatMap() = {
     val someTwo = Some(2)
     println(someTwo.map( i => i*i ))                    // Some(4)
     println(someTwo.map( i => "Value : %d".format(i) )) // Some(Value : 2)
@@ -11,5 +12,24 @@ object Basics {
     println(someTwo.map( i => Some(i) ))                // Some(Some(2))
     println(someTwo.map( i => Some(i) ).flatten)        // Some(2)
     println(someTwo.flatMap( i => Some(i) ))            // Some(2)
+  }
+
+  def optInt: Option[Int] = {
+    import scala.util.Random._
+    //if (nextBoolean()) Some(nextInt()%100) else None
+    if (nextBoolean()) Some(2) else None
+  }
+
+  def forComprehension() = {
+    // nested maps
+    val a = optInt.flatMap(i => optInt.map(j => i + j))
+    println(a)  // Some(4) or None
+
+    // simpler to use for comprehension
+    val b = for {
+      i <- optInt  // return type is determined by the first one
+      j <- optInt
+    } yield i + j
+    println(b) // Some(4) or None
   }
 }
