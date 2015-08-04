647ed3a1de9dffc2da0d93c7c415af6a4bed2a2a
Shajan Dasan <sdasan@twitter.com>
2015-08-04 16:36:18 -0700

    Rename

--- java/guava/mk.sh
+++ java/guava/mk.sh
@@ -1,3 +1,3 @@
 JARS=~/.m2/repository/com/google/guava/guava/17.0/guava-17.0.jar
-javac -cp $JARS Guava.java
-java -cp $JARS:. Guava
+javac -cp $JARS CustomRateLimiter.java
+java -cp $JARS:. CustomRateLimiter
