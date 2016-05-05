49b271758da7be536e1e511511181e73d33b6585
Shajan Dasan <sdasan@gmail.com>
2016-05-04 20:18:17 -0700

    Cleanup sort

--- algorithms/Sort.java
+++ algorithms/Sort.java
@@ -3,29 +3,31 @@ import java.util.Random;
 
 class Sort {
   public static void main(String[] args) {
-/*
-    int[] a = new int[]{10, 5, 63, 84, 63, 5, 49, 77, 50, 7, 40, 98, 15};
-    System.out.println(String.format("Before isSorted:%s", isSorted(a)));
-    qsort(a, 0, a.length - 1);
-    print(a, 0, a.length - 1);
-    System.out.println(String.format("After isSorted:%s", isSorted(a)));
-*/
-    benchmark(1000000);
+    benchmark(10000000, 10);
   }
 
-  public static void benchmark(int count) {
+  public static void benchmark(int size, int samples) {
     Random rand = new Random();
-    int[] base = new int[count];
-    for (int i=0; i<count; ++i) {
+    int[] base = new int[size];
+    for (int i=0; i<size; ++i) {
       base[i] = rand.nextInt();
     }
 
-    for (int i=0; i<10; ++i) {
-      if (isSorted(base))
-        System.out.println("Base is already sorted!");
+    for (int i=0; i<samples; ++i) {
       long start = System.currentTimeMillis();
-      Arrays.sort(base.clone());
+
+      int[] a = base.clone();
+      Arrays.sort(a);
       System.out.println(String.format("Array.sort ms : %d", System.currentTimeMillis() - start));
+      if (!isSorted(a))
+        System.out.println("Not sorted!");
+
+      a = base.clone();
+      start = System.currentTimeMillis();
+      qsort(a);
+      System.out.println(String.format("qsort ms : %d", System.currentTimeMillis() - start));
+      if (!isSorted(a))
+        System.out.println("Not sorted!");
     }
   }
 
@@ -44,42 +46,34 @@ class Sort {
     return true;
   }
 
+  public static void qsort(int[] a) {
+    qsort(a, 0, a.length - 1);
+  }
+
   public static void swap(int[] a, int i, int j) {
-    //System.out.println(a[i] + " <--> " + a[j]);
     int tmp = a[i];
     a[i] = a[j];
     a[j] = tmp;
   }
 
   public static void qsort(int[] a, int first, int last) {
-    //print(a, first, last);
-    if (first >= last)
-      return;
-
-    int pivot = a[first];
-    int i = first + 1;
+    int pivot = a[first + (last - first)/2];
+    int i = first;
     int j = last;
 
-    while (i < j) {
-      while (i < last && a[i] < pivot) ++i;
-      while (j > first && a[j] > pivot) --j;
-      if (i < j) {
+    while (i <= j) {
+      while (a[i] < pivot) ++i;
+      while (a[j] > pivot) --j;
+      if (i <= j) {
         swap(a, i, j);
         ++i;
         --j;
       }
     }
 
-    //print(a, first, last);
-    int pos = i;
-
-    if (a[pos] > pivot)
-      --pos;
-
-    if (pos > first && pivot > a[pos])
-      swap(a, first, pos);
-
-    qsort(a, first, pos-1);
-    qsort(a, pos+1, last);
+    if (first < j) 
+      qsort(a, first, j);
+    if (i < last) 
+      qsort(a, i, last);
   }
 }
