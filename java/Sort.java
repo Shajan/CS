da351a145ff4659047a98ebeb9c77f0d9727843a
Shajan Dasan <sdasan@twitter.com>
2016-05-20 16:50:06 -0700

    Add more examples and notes

---
+++ java/Sort.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Sort {
+  public static void main(String args[]) {
+    List<Integer> a = Arrays.asList(100, 20, 30, 150, 200, 175, 30, 210);
+    Collections.sort(a, new NumberComparator());
+    for (int i : a) {
+      System.out.println(i);
+    }
+  }
+
+  static class NumberComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer i1, Integer i2) {
+      // Natural sort order: larger value is greater
+      return i1 - i2;
+    }
+  }
+}
