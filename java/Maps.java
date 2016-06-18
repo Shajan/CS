57b310bd414a11f92e60853b65c607a900542423
Shajan Dasan <sdasan@twitter.com>
2016-06-17 17:05:58 -0700

    Check immutability

--- java/Maps.java
+++ java/Maps.java
@@ -1,11 +1,14 @@
-import java.util.Map;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.TreeMap;
-import java.util.LinkedHashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 class Maps {
   public static void main(String args[]) {
+/*
     Map<String,String> hm = new HashMap<String,String>();
     Map<String,String> tm = new TreeMap<String,String>();
     Map<String,String> lm = new LinkedHashMap<String,String>();
@@ -22,6 +25,9 @@ class Maps {
 
     System.out.println("HashMap Iterator");
     print(hm);
+*/
+
+    copy_semantics();
   }
 
   public static void populate(Map<String,String> m) {
@@ -46,6 +52,29 @@ class Maps {
     while (iter.hasNext()) {
       Map.Entry entry = (Map.Entry) iter.next();
       System.out.println(entry.getKey() + " --> " + entry.getValue());
+    }
+  }
+
+  public static void copy_semantics() {
+    Map<String,String> hm = new HashMap<String,String>();
+    populate(hm);
+
+    // Check if key set is mutable, vs. a clone
+    Set<String> keys = hm.keySet();
+    try {
+      keys.add("foobar");
+      System.out.println("Error! Map.keySet() is mutable");
+    } catch (UnsupportedOperationException ex) {
+      System.out.println("Map.keySet() is unmutable");
+    }
+
+    // Check if value is mutable, vs. a clone
+    Collection<String> values = hm.values();
+    try {
+      values.add("baz");
+      System.out.println("Error! Map.value() is mutable");
+    } catch (UnsupportedOperationException ex) {
+      System.out.println("Map.value() is unmutable");
     }
   }
 }
