04e58d1d13d551ed411450fa5dcf3c527f2f724b
Shajan Dasan <sdasan@twitter.com>
2017-01-16 11:40:18 -0800

    Regex example

--- java/Regex.java
+++ java/Regex.java
@@ -2,8 +2,21 @@ import java.util.regex.*;
 
 class Regex {
   public static void main(String args[]) {
+/*
     Pattern p = Pattern.compile(args[0]);
     Matcher m = p.matcher(args[1]);
     System.out.println(String.format("regex %s, input %s, matches %s", args[0], args[1], m.matches()));
+*/
+
+    Pattern p = Pattern.compile("^.*[0-9]*.*$");
+    Matcher m = p.matcher("abc123xyz456");
+    System.out.println(m.matches()); // prints true
+
+    p = Pattern.compile("-?[0-9]+");
+    m = p.matcher("abc123xyz456xxx");
+
+    while (m.find()) {
+      System.out.println(m.group());
+    }
   }
 }
