432029f04fa14d1d8f94987521b5306512fda2de
Shajan Dasan <sdasan@twitter.com>
2017-04-23 11:44:49 -0700

    Add loops

--- java/kids/tutorial/src/com/company/Loops.java
+++ java/kids/tutorial/src/com/company/Loops.java
@@ -5,6 +5,39 @@ package com.company;
  */
 public class Loops {
     public static void test() {
+        whileLoop();
+    }
 
+    private static void whileLoop() {
+        //printAllNumbers(5);
+        //printBetween(1,10);
+        printAllEven(20);
+     }
+
+    private static void printAllNumbers(int a) {
+        int i = 0;
+
+        while (i < a) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+
+    private static void printBetween(int start, int end) {
+        int i=start + 1;
+
+        while (i<end) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+
+    private static void printAllEven(int a) {
+        int i = 0;
+
+        while (i < a) {
+            System.out.println(i);
+            i = i + 2;
+        }
     }
 }
