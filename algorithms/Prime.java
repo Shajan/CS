8cd338605a5f43d33057d1aa4d876a14e25b9aea
Shajan Dasan <sdasan@twitter.com>
2017-08-07 15:14:07 -0700

    Add binary operators

--- algorithms/Prime.java
+++ algorithms/Prime.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Prime {
   private static List<Long> primes;
+
   public static void main(String[] args) {
-    primes = new ArrayList<Long>();
-    primes.add(2l);
+    generatePrimes(100);
 
-    for (long l=3; l<100; ++l) {
-      if (isPrime(l))
-        primes.add(l);
-    }
+    print("Primes :", primes);
+    print("Binary Sum :", binarySum(primes));
+    print("Binary Diff :", binaryDiff(primes));
+  }
 
-    for (long prime : primes) {
-      System.out.print(prime);
-      System.out.print(",");
+  private static void print(String s, List<Long> list) {
+    System.out.print(s);
+    for (long l : list) {
+      System.out.print(l);
+      System.out.print(' ');
     }
     System.out.println();
-
-    computePrimes(2, 3, 1);
-    computePrimes(3, 5, 2);
   }
 
   private static boolean isPrime(long l) {
@@ -29,7 +28,36 @@ class Prime {
     return true;
   }
 
-  private static void computePrimes(long a, long b, long c) {
-    System.out.println(String.format("%d, %d, %d ==> %d, %d", a, b, c, (a * b - c), (a * b + c)));
+  private static void generatePrimes(long max) {
+    primes = new ArrayList<Long>();
+
+    for (long l=2; l<max; ++l) {
+      if (isPrime(l))
+        primes.add(l);
+    }
+  }
+
+  private static List<Long> binarySum(List<Long> input) {
+    List<Long> output = new ArrayList<Long>();
+
+    long a = input.get(0);
+    for (int i=1; i<input.size(); ++i) {
+      long b = input.get(i);
+      output.add(a + b);
+      a = b;
+    }
+    return output;
+  }
+
+  private static List<Long> binaryDiff(List<Long> input) {
+    List<Long> output = new ArrayList<Long>();
+
+    long a = input.get(0);
+    for (int i=1; i<input.size(); ++i) {
+      long b = input.get(i);
+      output.add(b - a);
+      a = b;
+    }
+    return output;
   }
 }
