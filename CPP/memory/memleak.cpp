f51f26871ec9c103b84cc95835471373da349f77
Shajan Dasan <sdasan@twitter.com>
2019-03-29 16:56:35 -0700

    mtrace for leak detection

--- CPP/memory/memleak.cpp
+++ CPP/memory/memleak.cpp
@@ -1,9 +1,18 @@
 /*
- * References
+ * References : Using valgrind - runtime only checks
  *  https://www.gnu.org/software/libc/manual/html_node/Allocation-Debugging.html
  *  http://valgrind.org/docs/manual/ms-manual.html
+ *
+ * Reference : Using mtrace (not required for valgrind) - need code change at compile time
+ *  http://man7.org/linux/man-pages/man3/mtrace.3.html
  */
 #include <stdlib.h>
+
+#ifdef M_CHECK
+#include <mcheck.h>
+#else
+#define mtrace() 
+#endif
 
 void leakA() {
   malloc(40000);
@@ -18,6 +27,7 @@ int main(void) {
   int i;
   void* a[10];
 
+  mtrace();
   for (i = 0; i < 10; i++) {
      a[i] = malloc(1000);
   }
--- CPP/memory/mk.sh
+++ CPP/memory/mk.sh
@@ -7,26 +7,35 @@ set -e
 #    see file massif.out.<pid> after running
 #    ms_print massif.out.<pid> for better readability
 #  valgrind --tool=memcheck --leak-check=full ./memleak
+#
+# Compile using C++ sources
+# -g : Source level debug info
+
+if [ "$1" == "init" ]; then
+  wget https://raw.githubusercontent.com/lattera/glibc/master/malloc/mtrace.pl
+fi
 
 if [ "$1" == "clean" ]; then
-  rm -rf memleak
+  rm -rf memleak.valgrind
+  rm -rf memleak.mtrace
   rm -rf memleak.dSYM
   rm -rf massif.out.*
   exit 0
 fi
 
-if [ "$1" == "run" ]; then
+if [ "$1" == "valgrind" ]; then
+  clang++ -g -o memleak.valgrind memleak.cpp
   #valgrind --tool=massif ./memleak
   valgrind --tool=memcheck --leak-check=full ./memleak
   exit 0
 fi
 
-if [ "$1" == "compile" ]; then
-# Compile using C++ sources
-# -g : Source level debug info
-  clang++ -g -o memleak memleak.cpp
+if [ "$1" == "mtrace" ]; then
+  # run on linux
+  gcc -g -DM_CHECK=1 -o memleak.mtrace memleak.cpp
+  export MALLOC_TRACE=./mtrace.txt
+  ./memleak.mtrace
+  # The downloaded perl file does not appear to work
+  perl ./mtrace.pl ./memleak.mtrace $MALLOC_TRACE
   exit 0
 fi
-
-clang++ -g -o memleak memleak.cpp
-valgrind --tool=memcheck --leak-check=full ./memleak
