1e08b3647d5a0e25627b73b0fb5596dcdfcc3142
Shajan Dasan <sdasan@twitter.com>
2020-04-15 16:54:25 -0700

    Add more vararg usage

--- scala/Vararg.scala
+++ scala/Vararg.scala
@@ -1,18 +1,35 @@
+// https://www.lorrin.org/blog/2011/10/04/scalas-missing-splat-operator/
+
 object Vararg {
   def main(args: Array[String]): Unit = {
     process()
     process("a", "b", "c")
 
     val seq = Seq("ABC", "DEF")
-    process(seq:_*)
+    process(seq: _*)
+    println("....")
 
     val array = Array("X", "Y", "Z")
-    process(array:_*)
+    process(array: _*)
+    println("....")
+
+    val list = List("l1", "l2")
+    process(list: _*)
+    println("....")
+
+    // Don't know why anyone does this, adding examnple here
+    // just in case one comes across this code and needs to understand
+    // what it does.
+    val args = (10, 5)
+    val result = mul _ tupled args 
+    println(result)
   }
 
+  // Unclear why this is useful, just take a sequence instead?
   // * at the end means any number of String
   def process(str: String*): Unit = {
     str.foreach(println)
-    println("....")
   }
+
+  def mul(a: Int, b: Int): Int = { a * b }
 }
