e6d8678d1a8d976034c605baab478f2551a171ff
Shajan <sdasan@gmail.com>
2021-03-02 17:54:07 -0800

    Support for HTTP POST using Django

--- web/Django/helloworld/calc/urls.py
+++ web/Django/helloworld/calc/urls.py
@@ -5,5 +5,12 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'), # Default page
   path('static', views.home, name='home'),
-  path('dynamic', views.dynamic, name='dynamic')
+  path('dynamic', views.dynamic, name='dynamic'),
+  path('dynamicUsingBase', views.dynamicUsingBase, name='dynamicUsingBase'),
+
+  path('getDataForm', views.getDataForm, name='getDataForm'),
+  path('getDataCompute', views.getDataCompute, name='getDataCompute'),
+
+  path('postDataForm', views.postDataForm, name='postDataForm'),
+  path('postDataCompute', views.postDataCompute, name='postDataCompute'),
 ]
--- web/Django/helloworld/calc/views.py
+++ web/Django/helloworld/calc/views.py
@@ -10,3 +10,44 @@ def home(request):
 # Use template/dynamic.html
 def dynamic(request):
   return render(request, 'dynamic.html', {'name' : 'Shajan'})
+
+# Use template/dynamicUsingBase.html
+def dynamicUsingBase(request):
+  return render(request, 'dynamicUsingBase.html', {'name' : 'Shajan'})
+
+def getDataForm(request):
+  return render(request, 'getDataForm.html')
+
+# Getting values from http 'GET' request
+def getDataCompute(request):
+  val1 = request.GET['num1']
+  val2 = request.GET['num2']
+
+  try:
+    val1 = int(val1)
+    val2 = int(val2)
+    result = val1 + val2
+    result = str(result)
+  except:
+    result = "Use numbers"
+
+  return render(request, 'result.html', {'result' : result})
+
+
+def postDataForm(request):
+  return render(request, 'postDataForm.html')
+
+# Posting values from http
+def postDataCompute(request):
+  val1 = request.POST['num1']
+  val2 = request.POST['num2']
+
+  try:
+    val1 = int(val1)
+    val2 = int(val2)
+    result = val1 + val2
+    result = str(result)
+  except:
+    result = "Use numbers"
+
+  return render(request, 'result.html', {'result' : result})
