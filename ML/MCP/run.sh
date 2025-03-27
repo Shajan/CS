08ef84b896d08c780fffedb6aae35559106df1bb
Shajan Dasan <sdasan@gmail.com>
2025-03-27 00:05:20 -0700

    Update script

--- ML/MCP/run.sh
+++ ML/MCP/run.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# Use first argument if provided, otherwise default to 'server.py'
+# Use first argument if provided, otherwise default to 'sample1.py'
 SCRIPT_TO_RUN="${1:-sample1.py}"
 
 # Get the directory of the script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Push to that directory
+# Change to the directory where the .venv is
 pushd "${SCRIPT_DIR}" > /dev/null
 
-python -m venv .venv
 source .venv/bin/activate
 
 # Start the server
 python "$SCRIPT_TO_RUN"
 
+# Go back to where we were 
 popd > /dev/null
-
